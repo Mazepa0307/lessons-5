891f5eaec90b3e984d491e9ef60aed1d30c5521a
Mazepa0307 <danekmazepa@gmail.com>
2024-08-19 17:24:58 +0200

    Update lessons5.py3.py

--- lessons5.py3.py
+++ lessons5.py3.py
@@ -3,12 +3,12 @@ def create_hashtag(input_string):
     translator = str.maketrans('', '', string.punctuation + '')
     cleaned_string = input_string.translate(translator)
 
-    hashtag = cleaned_string.title()
+    hashtag = cleaned_string.replace('','').title()
+    hashtag = f'#{hashtag}'
 
     if len(hashtag) > 140:
         hashtag = hashtag[:140]
 
-    hashtag = f'#{hashtag}'
     return hashtag
 
 user_input = input('Enter a string: ')
