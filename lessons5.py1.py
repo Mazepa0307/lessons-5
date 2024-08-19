f0c9776d2652a64973c63b90b2f868cf197450f0
Mazepa0307 <danekmazepa@gmail.com>
2024-08-19 17:17:56 +0200

    Update lessons5.py1.py

--- lessons5.py1.py
+++ lessons5.py1.py
@@ -5,13 +5,12 @@ def is_valid_variable_name(name):
     if name in keyword.kwlist or name [0].isdigit():
         return False
     for char in name:
-        if char in string.punctuation and char !='_':
+        if char.isupper() or (char in string.punctuation and char !='_'):
             return False
-    for char in name:
-        if char.isupper():
-            return False
+
     if name.count('_') > 1:
         return False
     return True
+
 user_input = input("Enter a variable name: ")
 print(is_valid_variable_name(user_input))
